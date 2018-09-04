c076bcfc75d663949b26c892e88e74a52ffb8c40
Vittorio Ballestra <vittorio.ballestra@gmail.com>
2018-09-04 17:47:50 +0200

    initial

---
+++ src/main/groovy/org/acme/security/playground/playground/PlaygroundApplication.groovy
@@ -0,0 +1,12 @@
+package org.acme.security.playground.playground
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PlaygroundApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PlaygroundApplication, args
+	}
+}
